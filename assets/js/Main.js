72e8b2a1f7288abc868279a4cf9e7d1976da1e44
PeterS <pstylu@gmail.com>
2018-12-19 19:26:29 +0100

    Revert "revert"
    This reverts commit abebc6daccb06f5457847defe45eac16d2bd81e5.

--- assets/js/Main.js
+++ assets/js/Main.js
@@ -1,3 +1,138 @@
+let canvas = document.getElementById("myCanvas");
+let pointer = document.querySelector(".pointer");
+let messager = document.querySelector(".messager-container");
+let waitingScreenDiv = document.querySelector(".waiting-screen");
+
+let camera0, scene0, scene1, renderer, stats, controls, audioListener;
+let loadingManager, textureLoader, gltfLoader, FBXLoader, audioLoader;
+let clock, delta;
+let currentLevel = 0;
+
+// Files
+let Textures = {
+	grass: null,
+	walls: {
+		roughnessMap: null,
+		metalnessMap: null,
+	},
+	floor: {
+		lightMap: null,
+		alphaMap: null, 
+		blackMap: null,
+	},
+	radio: {
+		normalMap: null,
+		aoMap: null,
+		specularMap: null,
+	},
+	Creeper: {
+		skin: null,
+	},
+	loadingPics: [],
+};
+let Sounds = {
+	footsteps: null,
+	doorOpening: null,
+	december: null,
+	quietGrowl: null,
+};
+
+
+let shadows = false;
+let box3helpers = false;
+
+// LOADING SCREEN
+let loadingReady = false;
+let ls = {
+	scene: new THREE.Scene(),
+	camera: new THREE.PerspectiveCamera( 55, window.innerWidth/window.innerHeight, 0.1, 1000 ),
+	sphere: new THREE.Mesh( 
+		new THREE.SphereBufferGeometry(1), 
+		new THREE.MeshBasicMaterial({ color: 0xff0040, wireframe: true }),
+	),
+};
+
+// WAITING SCREEN
+let ws = {
+	scene: new THREE.Scene(),
+	camera: new THREE.OrthographicCamera( -100 * window.innerWidth/window.innerHeight , 100 * window.innerWidth/window.innerHeight , 100 , -100 , 0.1, 100 ),
+	sphere: new THREE.Mesh( 
+		new THREE.SphereBufferGeometry(1), 
+		new THREE.MeshBasicMaterial({ color: 0xff0040, wireframe: true }),
+	),
+};
+
+// PLAYER
+let playerStats = {
+	height: 5,
+	speed: 7.0,
+}
+let player = {
+	update: function(){
+		// just to avoid ifing existance of player being classed
+	}
+};
+
+// MONSTERS
+let Monsters = {
+	// Array of enemies
+	array: [],
+	// Info of enemies
+	Creeper: {
+		walkingSpeed: 4.0,
+	},
+};
+
+// GAME
+let GameState = {
+	progress: 0,
+};
+
+// LEVELS
+let Levels = [
+	{
+		name: "Level 0",
+		Lights: [],
+		scene: null,
+		staticCollideMesh: [],
+		interractiveItems: [],
+		lightHelpers: false,
+		events: [],
+		messages: {
+			instructions: {
+				text: `
+					Use [W][S][A][D] to move. </br>
+					Hold [Shift] to run or [C] to crouch. </br>
+					Press [Q] or [E] to look around. </br> 
+				`,
+				duration: 7000,
+			},
+			noLantern: {
+				text: `I feel like I'm forgetting something...`,
+				duration: 2000,
+			},
+			howToUseLantern: {
+				text: `Press [F] to switch on your glowstick`,
+				duration: 4000,
+			},
+		},
+	},
+	{
+		name: "Level 1",
+		Lights: [],
+		scene: null,
+		staticCollideMesh: [],
+		interractiveItems: [],
+		lightHelpers: true,
+		events: [],
+		messages: {
+			lockedDoor: {
+				text: `It's locked..`,
+				duration: 1000,
+			},
+		},
+	}
+];
 
 
 let init = function() {
@@ -36,9 +171,22 @@ let init = function() {
 	
 	
 	loadingManager = new THREE.LoadingManager();
+	loadingManager.onStart = function ( url, itemsLoaded, itemsTotal ) {
+		// console.log( 'Started loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.' );
+	};
 	loadingManager.onLoad = function ( ) {
+		setTimeout( function(){ 
+			loadingFinished();
+		}, 1000 );
+		Levels[0].init();
 		
-		loadingReady = true;
+		requestAnimationFrame( animate );
+	};
+	loadingManager.onProgress = function ( url, itemsLoaded, itemsTotal ) {
+		// console.log( 'Loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.' );
+	};
+	loadingManager.onError = function ( url ) {
+		console.log( 'There was an error loading ' + url );
 	};
 	
 	textureLoader = new THREE.TextureLoader( loadingManager );
@@ -50,13 +198,10 @@ let init = function() {
 	audioLoader = new THREE.AudioLoader( loadingManager );
 	FBXLoader = new THREE.FBXLoader( loadingManager );
 	
-	// fadeIn( waitingScreenDiv );
+	fadeIn( waitingScreenDiv );
 	initTextures();
 	initSounds();
 	loadModels();
-	
-	// loadingReady = false;
-	Levels[0].init();
 	
 }
 
@@ -105,7 +250,6 @@ Levels[0].init = function( pos, rot ){
 		}, 7000 )
 	}
 	
-	console.log( "Room: " );
 	console.log( Levels[0].scene );
 }
 
@@ -137,7 +281,7 @@ Levels[1].init = function( pos ){
 let loadModels = function(){
 
 	// gltfLoader.load( '/assets/models/Level_0_alt/glb/room1_ver2.glb',
-	/* gltfLoader.load( '/assets/models/Level_0/room1_ver2.gltf',
+	gltfLoader.load( '/assets/models/Level_0/room1_ver2.gltf',
 		function ( gltf ) {
 			
 			Levels[0].gltf = gltf;
@@ -147,7 +291,7 @@ let loadModels = function(){
 		}, function ( error ) {
 			console.log( 'Error happened: ' + error);
 		}
-	); */
+	);
 	
 	gltfLoader.load( '/assets/models/Level_1/corridor.gltf',
 		function ( gltf ) {
@@ -301,7 +445,7 @@ let spam = function(num){
 let animate = function( time ) {
 	
 	if( loadingReady == false ){
-		LoadingScreen();
+		// LoadingScreen();
 		// pointer.src = "assets/textures/pointer_empty.png";
 		requestAnimationFrame(animate);
 		return;
@@ -325,4 +469,4 @@ let animate = function( time ) {
 }
 
 init();
-requestAnimationFrame( animate );
+// requestAnimationFrame( animate );
